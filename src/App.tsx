df4a4f3ae52a048b5c7a321ae4da58e97e40cf44
anny <puchkovdd@gmail.com>
2022-08-19 15:50:20 +0300

    adede function

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Card, { CardVariant } from './components/Card';
 function App() {
   return (
     <div>
-      <Card variant={CardVariant.primary} width='200px' height='200px'>
+      <Card onClick={(num)=>{console.log("click",num)}} variant={CardVariant.primary} width='200px' height='200px'>
           <button>Кнопка</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,38 @@
-import React,{FC, PropsWithChildren} from 'react';
+import React, { FC, PropsWithChildren, useState } from "react";
 
-export enum CardVariant{
-    outlined = "outlined",
-    primary = "primary"
+export enum CardVariant {
+  outlined = "outlined",
+  primary = "primary",
 }
 
-
-interface CardProps extends  PropsWithChildren{
-    width?:string;
-    height?:string;
-    variant:CardVariant
+interface CardProps extends PropsWithChildren {
+  width?: string;
+  height?: string;
+  variant: CardVariant;
+  onClick: (num:number) => void;
 }
 
-const Card:FC<CardProps> = ({width,height,children,variant}) => {
-    return (
-        <div style={{width,height,border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
-            background:variant === CardVariant.primary ? 'lightgray': ''     
-        }}>
-            {children}
-        </div>
-    );
+const Card: FC<CardProps> = 
+({ 
+    width, 
+    height, 
+    children, 
+    variant,
+    onClick, 
+}) => {
+    const [state,setState] = useState(0);
+  return (
+    <div onClick={()=>{onClick(state)}}
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? "1px solid gray" : "none",
+        background: variant === CardVariant.primary ? "lightgray" : "",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Card;
